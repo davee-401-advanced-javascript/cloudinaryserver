1a15a347277111bdbb8af8585f2e88a21487d96f
Davee Sok <daveesok@gmail.com>
2021-01-22 21:35:17 -0800

    updated

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// require('dotenv').config();
-// let mongoose = require('mongoose');
-// let server = require('./server.js');
-
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-// });
-
-// server.start(process.env.PORT);
-
 
 require("dotenv").config();
 const express = require("express");
